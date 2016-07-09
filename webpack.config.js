5d6acb6cc8a8f1f6e023afa1a0e773d5b5b14896
Remon Georgy <remon.sherin@gmail.com>
2016-07-09 09:32:21 -0400

    Ignore some external modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,28 @@ module.exports = {
   entry: './public/src/app.js',
   output: {
     path: './public/bin',
+    // You must not specify an absolute path here.
     filename: 'bundle.js'
   },
+  // Set devtool to enable source map.
   devtool: false,
+  // Node.js and Music21j modules that webpack shouldb't bundlle.
+  externals: {
+    'fs': 'empty',
+    'child_process': 'empty',
+    'xmlhttprequest': 'empty',
+    'walk': 'empty',
+    'file': 'empty',
+    'directory': 'empty',
+    // Music21j dependencies.
+    'js-beautify': 'empty',
+    'MIDI': 'empty',
+    'jsonpickle': 'empty',
+    'vexflow': 'empty',
+    // Non-existing file that disturbs packing process.
+    'images/jq.png': 'empty',
+    './images/jq.png': 'empty'
+  },
   module: {
     loaders: [
       {
