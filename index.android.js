ac98058568ba58803ef968ecd5c998cfbb93992b
Remon Georgy <remon.sherin@gmail.com>
2016-07-11 18:02:08 -0400

    Initial attempt to get react native running

---
+++ index.android.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View
+} from 'react-native';
+
+// TODO useimport
+
+
+
+class player extends Component {
+  constructor(props) {
+    super(props);
+    
+    var _reactComponent = this;
+    var requirejs = require('requirejs');
+    // require(['./public/music21j/src/music21'], function () {
+    //   var stream = music21.tinyNotation.TinyNotation(_reactComponent.props.tinyNotation);
+    //     // TODO this method might be deprecated.
+    //     // @see music21.stream.Stream.renderScrollableCanvas();
+    //   stream.renderScrollableCanvas($('#can'));
+    //
+    //     // Toolbar
+    //   var toolBar = stream.getPlayToolbar();
+    //   console.log(toolBar);
+    // });
+  }
+
+  render() {
+    return (
+      <View className="player">
+        <Text>Click and listen</Text>
+        <View id="can"></View>
+      </View>
+    );
+  }
+}
+
+class playerApp extends Component {
+  render() {
+    return (
+      <player tinyNotation="g# a b d b a g">
+      </player>
+    );
+  }
+}
+
+AppRegistry.registerComponent('player', () => player);
