89484f61e346197a0de32996dc0e94aa2a14053e
Remon Georgy <remon.sherin@gmail.com>
2016-08-04 14:06:45 -0400

    Display music21j player via WebView component

--- index.android.js
+++ index.android.js
@@ -4,33 +4,36 @@
  * @flow
  */
 
-import requirejs from 'requirejs';
+// import requirejs from 'requirejs';
+// import music21 from './public/music21j/src/music21'
 import React, { Component } from 'react';
 import $ from 'jquery';
-import { AppRegistry,  Text,  View} from 'react-native';
+import { AppRegistry,  Text,  View, WebView} from 'react-native';
 
 class player extends Component {
   constructor(props) {
     super(props);
+    console.log(require);
+    console.log('HERE');
     
-    var _reactComponent = this;
-    require(['./public/music21j/src/music21'], function () {
-      var stream = music21.tinyNotation.TinyNotation(_reactComponent.props.tinyNotation);
-        // TODO this method might be deprecated.
-        // @see music21.stream.Stream.renderScrollableCanvas();
-      stream.renderScrollableCanvas($('#can'));
-      // Toolbar
-      var toolBar = stream.getPlayToolbar();
-      console.log(toolBar);
-    });
+    // require(['./public/music21j/src/music21'], function () {
+    //   var stream = music21.tinyNotation.TinyNotation(_reactComponent.props.tinyNotation);
+    //   // TODO this method might be deprecated.
+    //   // @see music21.stream.Stream.renderScrollableCanvas();
+    //   stream.renderScrollableCanvas($('#can'));
+    //   // Toolbar
+    //   var toolBar = stream.getPlayToolbar();
+    //   console.log(toolBar);
   }
+  // });
 
   render() {
     return (
-      <View className="player">
-        <Text>Click and listen</Text>
-        <View id="can"></View>
-      </View>
+      <WebView
+    //    source={{uri: 'http://localhost:3000'}}
+        //source={{uri: 'http://www.google.com', scalesPageToFit: true}}
+        source={{uri: 'http://10.0.2.2:3000', scalesPageToFit: true}}
+      />
     );
   }
 }
